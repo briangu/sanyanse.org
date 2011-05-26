399a4e380926795ea5392caeca71b7ab40bc9eb9
Brian Guarraci <brian.guarraci@gmail.com>
2011-05-25 18:00:52 -0700

    update ui to support profile, style.css, etc

--- public/signal.js
+++ public/signal.js
@@ -31,8 +31,8 @@
   var maxNodes = 12;
   var outstandingPalRequests = 0;
   var maxPalRequests = 2;
-  var maxColors = 4;
-
+  var maxColors = 6;
+  var maxTextLength = "David Einhorn, hedge-fund star, calls for Microsoft CEO Steve".length;
   var networkQuery = "network,0,1,2";
 
   colorIdx = 0;
@@ -213,17 +213,29 @@
       return;
     }
 
+    if (person.pictureUrl == undefined) {
+      trace("article missing profile URL: " + share.id);
+      return false;
+    }
+
     if (share.content.submittedUrl != undefined) {
       if (visitedMap[share.content.submittedUrl] != undefined) return false;
       visitedMap[share.content.submittedUrl] = true;
+    }
+
+    updateText = share.content.title;
+    if (updateText.length > maxTextLength)
+    {
+      updateText = updateText.substring(0,maxTextLength) + "...";
     }
 
     //share.content.thumbnailUrl
     var data = {
       color: -1,
       url: share.content.eyebrowUrl,
+      profileUrl: person.siteStandardProfileRequest.url,
       img: person.pictureUrl,
-      text: share.content.title,
+      text: updateText,
       type: "update"
     };
 
@@ -263,8 +275,10 @@
       }
       else
       {
-        sys.addNode("facetName_"+value.code, {text: data.facetName, type:"facet", color:data.color, mass: 3});
-        sys.addEdge(update.updateKey, "facetName_"+value.code);
+        fndata = {text: data.facetName, type:"facet", color:data.color, mass: 2};
+        sys.addNode("facetName_"+value.code, fndata);
+        edge = sys.addEdge(update.updateKey, "facetName_"+value.code);
+        edge.length = 0.2;
         facetMap[value.code] = new Array();
       }
 
@@ -323,7 +337,7 @@
         gfx.clear();
         sys.eachEdge(function(edge, p1, p2)
                      {
-//                       if (edge.source.data.alpha * edge.target.data.alpha == 0) return
+                       if (edge.target.data.type == "facet") return;
                        gfx.line(p1, p2, {stroke:"#7f7f7f", width:1, alpha: 0.5})
                      })
         sys.eachNode(function(node, pt)
@@ -334,25 +348,13 @@
                          var id = "div_" + node.name;
 
                          if (node.data.type == "update") {
-                           var imgTag;
-                           var textTag;
-                           if (node.data.img == undefined
-                               || node.data.img == "https://www.linkedin.com/media-proxy/ext?w=80&h=100&hash=nwPjKTbWxd29U2O8wALPidJSp2o%3D&url=http%3A%2F%2Fstatic01.linkedin.com%2Fscds%2Fcommon%2Fu%2Fimg%2Flogos%2Flogo_signal_120x120.png"
-                               || node.data.img == "https://www.linkedin.com/media-proxy/ext?w=80&h=100&hash=ttnfXkxr9kjtq1O4pggdcZC2HHs%3D&url=http%3A%2F%2Fmedia03.linkedin.com%2Fmedia%2Fp%2F3%2F000%2F0bc%2F1d8%2F393c029.png")
-                           {
-                             imgTag = "";
-                             textTag = "<p class='updatepni'>" + node.data.text + "</p>";
-                           }
-                           else
-                           {
-                             imgTag = "<img id='divi_"+id+"' class='updateimg' src='" + node.data.img + "'/>";
-                             textTag = "<p class='updatep'>" + node.data.text + "</p>";
-                           }
+                           var imgTag = "<img id='divi_"+id+"' class='updateimg' src='" + node.data.img + "'/>";
+                           var textTag = "<p class='updatep'>" + node.data.text + "</p>";
                            var closeTag = "<img id='img_"+id+"' class='closeTag' src='close-cross.png'/>"
 
                            var div = jQuery('<div/>', {
                              id: id,
-                             class: "update" + node.data.color,
+                             class: "updateBase update" + node.data.color,
                              html: imgTag + textTag + closeTag,
                              mouseover: function() {
                                $('#img_'+id).css('visibility', 'visible');
@@ -366,17 +368,17 @@
                              {
                                if (e.target.id == "img_"+id) {
                                  deleteNode(node);
+                               } else if (e.target.id == "divi_" + id) {
+                                 window.open(node.data.profileUrl);
                                } else {
                                  window.open(node.data.url);
                                }
                              }
                            });
                          } else {
-                           var facetNameTag = "<p class='updateFacetName"+ node.data.color + "'>" + node.data.text + "</p>";
-
+                           var facetNameTag = "<p class='updateFacetNameBase updateFacetName"+ node.data.color + "'>" + node.data.text + "</p>";
                            var div = jQuery('<div/>', {
                              id: id,
-//                             class: "facetLabel"+node.data.color,
                              html: facetNameTag
                            });
                          }
@@ -438,10 +440,6 @@
             if (dragged.node !== null)
             {
               dragged.node.fixed = false
-              if (!dragged.node.data.moved)
-              {
-//                deleteNode(dragged.node);
-              }
             }
             dragged.node.tempMass = 1000
             dragged = null
@@ -499,9 +497,7 @@
 
   $(document).ready(function()
                     {
-                      sys = arbor.ParticleSystem(700, 250, 0.34, true, 55, 0.005, 0.4) // create the system with sensible repulsion/stiffness/friction
-//                          sys = arbor.ParticleSystem();
-//                          sys.parameters({stiffness:500, repulsion:50, gravity:true, dt:0.005});
+                      sys = arbor.ParticleSystem(1000, 600, 0.5, true, 120, 0.005, 0.4) // create the system with sensible repulsion/stiffness/friction
                       sys.renderer = Renderer("#sitemap")
 
                       // load the data into the particle system as is (since it's already formatted correctly for .grafting)
@@ -512,156 +508,6 @@
 
                       onLinkedInLoad();
                     })
-
-  var industryNameMap = {
-  1: "Defense And Space",
-  3: "Computer Hardware",
-  4: "Computer Software",
-  5: "Computer Networking",
-  6: "Internet",
-  7: "Semiconductors",
-  8: "Telecommunications",
-  9: "Law Practice",
-  10: "Legal Services",
-  11: "Management Consulting",
-  12: "Biotechnology",
-  13: "Medical Practice",
-  14: "Hospital And Health Care",
-  15: "Pharmaceuticals",
-  16: "Veterinary",
-  17: "Medical Equipment",
-  18: "Cosmetics",
-  19: "Apparel And Fashion",
-  20: "Sporting Goods",
-  21: "Tobacco",
-  22: "Supermarkets",
-  23: "Food Production",
-  24: "Consumer Electronics",
-  25: "Consumer Goods",
-  26: "Furniture",
-  27: "Retail",
-  28: "Entertainment",
-  29: "Gambling And Casinos",
-  30: "Leisure And Travel",
-  31: "Hospitality",
-  32: "Restaurants",
-  33: "Sports",
-  34: "Food And Beverages",
-  35: "Motion Pictures And Film",
-  36: "Broadcast Media",
-  37: "Museums And Institutions",
-  38: "Fine Art",
-  39: "Performing Arts",
-  40: "Recreational Facilities And Services",
-  41: "Banking",
-  42: "Insurance",
-  43: "Financial Services",
-  44: "Real Estate",
-  45: "Investment Banking/venture",
-  46: "Investment Management",
-  47: "Accounting",
-  48: "Construction",
-  49: "Building Materials",
-  50: "Architecture  Planning",
-  51: "Civil Engineering",
-  52: "Aviation And Aerospace",
-  53: "Automotive",
-  54: "Chemicals",
-  55: "Machinery",
-  56: "Mining And Metals",
-  57: "Oil And Energy",
-  58: "Shipbuilding",
-  59: "Utilities",
-  60: "Textiles",
-  61: "Paper And Forest Products",
-  62: "Railroad Manufacture",
-  63: "Farming",
-  64: "Ranching",
-  65: "Dairy",
-  66: "Fishery",
-  67: "Primary/secondary",
-  68: "Higher Education",
-  69: "Education Management",
-  70: "Research",
-  71: "Military",
-  72: "Legislative Office",
-  73: "Judiciary",
-  74: "International Affairs",
-  75: "Government Administration",
-  76: "Executive Office",
-  77: "Law Enforcement",
-  78: "Public Safety",
-  79: "Public Policy",
-  80: "Marketing And Advertising",
-  81: "Newspapers",
-  82: "Publishing",
-  83: "Printing",
-  84: "Information Services",
-  85: "Libraries",
-  86: "Environmental Services",
-  87: "Package/freight Delivery",
-  88: "Individual And Family Services",
-  89: "Religious Institutions",
-  90: "Civic And Social Organization",
-  91: "Consumer Services",
-  92: "Transportation/trucking/railroad",
-  93: "Warehousing",
-  94: "Airlines/aviation",
-  95: "Maritime",
-  96: "Information Technology And Services",
-  97: "Market Research",
-  98: "Public Relations",
-  99: "Design",
-  100: "Non-profit Organization Management",
-  101: "Fundraising",
-  102: "Program Development",
-  103: "Writing And Editing",
-  104: "Staffing And Recruiting",
-  105: "Professional Training",
-  106: "Venture Capital",
-  107: "Political Organization",
-  108: "Translation And Localization",
-  109: "Computer Games",
-  110: "Events Services",
-  111: "Arts And Crafts",
-  112: "Electrical And Electronic Manufacturing",
-  113: "Online Publishing",
-  114: "Nanotechnology",
-  115: "Music",
-  116: "Logistics And Supply Chain",
-  117: "Plastics",
-  118: "Computer And Network Security",
-  119: "Wireless",
-  120: "Alternative Dispute Resolution",
-  121: "Security And Investigations",
-  122: "Facilities Services",
-  123: "Outsourcing/offshoring",
-  124: "Health: Wellness And Fitness",
-  125: "Alternative Medicine",
-  126: "Media Production",
-  127: "Animation",
-  128: "Commercial Real Estate",
-  129: "Capital Markets",
-  130: "Think Tanks",
-  131: "Philanthropy",
-  132: "E-learning",
-  133: "Wholesale",
-  134: "Import And Export",
-  135: "Mechanical Or Industrial Engineering",
-  136: "Photography",
-  137: "Human Resources",
-  138: "Business Supplies And Equipment",
-  139: "Mental Health Care",
-  140: "Graphic Design",
-  141: "International Trade And Development",
-  142: "Wine And Spirits",
-  143: "Luxury Goods And Jewelry",
-  144: "Renewables And Environment",
-  145: "Glass: Ceramics And Concrete",
-  146: "Packaging And Containers",
-  147: "Industrial Automation",
-  148: "Government Relations"};
-
 })(this.jQuery)
 
 
